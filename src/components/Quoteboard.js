71648791d30abb030d58dc5e317ae561e52ceb18
kirstenjalvingh <kirsten.jalvingh@gmail.com>
2019-11-14 14:53:15 +0100

    all english quotes loaded and sorted

--- src/components/Quoteboard.js
+++ src/components/Quoteboard.js
@@ -3,18 +3,32 @@ import QuoteBlock from "./QuoteBlock";
 
 
 export default class Quoteboard extends Component {
-
+  
     state = {
-        quotes: [
-            { id: 1, author: "Henry Ford", quoteText: "If we’d asked the customers what they wanted, they would have said “faster horses”.", imageUrl: "https://images.unsplash.com/photo-1573020568125-d15af9c3e777?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixlib=rb-1.2.1&q=80&w=200" },
-            { id: 2, author: "Fred Brooks", quoteText: "What one programmer can do in one month, two programmers can do in two months.", imageUrl: "https://images.unsplash.com/photo-1572391642723-93686163161e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixlib=rb-1.2.1&q=80&w=200" },
-            { id: 3, author: "Douglas Crockford", quoteText: "It turns out that style matters in programming for the same reason that it matters in writing. It makes for better reading.", imageUrl: "https://images.unsplash.com/photo-1572016874827-00983478b54e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixlib=rb-1.2.1&q=80&w=200" },
-        ]
+        quotes : null
+      }
+    async componentDidMount(){
+        this.getQuote();
     }
 
+    getQuote = async() => {
+        const response = await fetch("https://programming-quotes-api.herokuapp.com/quotes/lang/en");
+        const json = await response.json();
+        // setState
+        //this.setState({itemData : [...this.state.itemData, { ...json, imgUrl: `https://source.unsplash.com/random/${790 + Math.round(Math.random() * 50)}x600`}]})
+        this.setState({quotes : json})
+    };
+
+
+
+
     render() {
+        if (this.state.quotes == null){
+            return <div>Even wachten...</div>
+        }
         const quotes_copy = [...this.state.quotes];
-        quotes_copy.sort((a, b) => b.author - a.author);
+        quotes_copy.sort((a, b) => a.author.localeCompare(b.author));
+        
 
         return (
             <div className="quoteboard">
@@ -34,8 +48,8 @@ export default class Quoteboard extends Component {
         return (
             <QuoteBlock id={quote.id}
             author={quote.author}
-            quoteText={quote.quoteText}
-            imageUrl={quote.imageUrl}
+            quoteText={quote.en}
+            // imageUrl={quote.imageUrl}
             key={quote.id} />
                 
         );
